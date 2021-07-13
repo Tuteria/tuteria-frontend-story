a7d4a58c75a5ec82267428018042e98d9a667495
usmanoa <usmanakorede@gmail.com>
2021-07-13 20:20:02 +0100

    feat: add menu view story

--- src/stories/primitives.stories.tsx
+++ src/stories/primitives.stories.tsx
@@ -2,8 +2,22 @@ import React from "react";
 import { IonApp } from "@ionic/react";
 import { IonReactRouter } from "@ionic/react-router";
 
-import { Tabs, Page } from "@tuteria/mobile-lib/src/primitives";
-import { ellipse, square, triangle } from "ionicons/icons";
+import { TabView, Page, MenuView } from "@tuteria/mobile-lib/src/primitives";
+import {
+  ellipse,
+  square,
+  triangle,
+  archiveOutline,
+  archiveSharp,
+  heartOutline,
+  heartSharp,
+  mailOutline,
+  mailSharp,
+  paperPlaneOutline,
+  paperPlaneSharp,
+  trashOutline,
+  trashSharp,
+} from "ionicons/icons";
 import ExploreContainer from "@tuteria/mobile-lib/src/components/ExploreContainer";
 
 export default {
@@ -52,6 +66,75 @@ const tabs = [
   { path: "/tab3", component: Tab3, id: "tab3", icon: square, title: "Tab 3" },
 ];
 
+const Menu1: React.FC = () => {
+  return (
+    <Page title="Menu 1" fullscreen>
+      <ExploreContainer name="Menu 1 page" />
+    </Page>
+  );
+};
+
+const Menu2: React.FC = () => {
+  return (
+    <Page title="Menu 2" fullscreen>
+      <ExploreContainer name="Menu 2 page" />
+    </Page>
+  );
+};
+
+const Menu3: React.FC = () => {
+  return (
+    <Page title="Menu 3" fullscreen>
+      <ExploreContainer name="Menu 3 page" />
+    </Page>
+  );
+};
+
+const Menu4: React.FC = () => {
+  return (
+    <Page title="Menu 4" fullscreen>
+      <ExploreContainer name="Menu 4 page" />
+    </Page>
+  );
+};
+
+const menu = [
+  {
+    title: "Menu 1",
+    path: "/menu1",
+    component: Menu1,
+    iosIcon: mailOutline,
+    mdIcon: mailSharp,
+  },
+  {
+    title: "Menu 2",
+    path: "/menu2",
+    component: Menu2,
+    iosIcon: paperPlaneOutline,
+    mdIcon: paperPlaneSharp,
+  },
+  {
+    title: "Menu 3",
+    path: "/menu3",
+    component: Menu3,
+    iosIcon: heartOutline,
+    mdIcon: heartSharp,
+  },
+  {
+    title: "Menu 4",
+    path: "/menu4",
+    component: Menu4,
+    iosIcon: archiveOutline,
+    mdIcon: archiveSharp,
+  },
+];
+
 export const TabsExample = () => {
-  return <Tabs tabs={tabs} />;
+  return <TabView tabs={tabs} />;
+};
+
+export const MenuExample = () => {
+  return (
+    <MenuView header="Profile" headerNote="tutor@email.com" menuItems={menu} />
+  );
 };
