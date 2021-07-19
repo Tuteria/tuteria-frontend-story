83f2b9d4761dd320fc12a35826b79b557adbfdc6
Malik Mahmud <almalikmahmud@gmail.com>
2021-07-19 16:51:43 +0100

    update WorkHistory component story

--- src/stories/forms.stories.tsx
+++ src/stories/forms.stories.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { IonApp } from "@ionic/react";
+import { IonApp, IonButton } from "@ionic/react";
 import WorkHistoryComponent from "@tuteria/mobile-lib/src/forms/WorkHistory";
 import "@ionic/react/css/core.css";
 import "@ionic/react/css/typography.css";
@@ -17,15 +17,32 @@ export default {
   ],
 };
 
-export const WorkHistory = () => (
-  <Box maxW="600px" m="auto" w="100%">
-    <WorkHistoryComponent
-      handleFormSubmit={(values) => console.log(values)}
-      values={{
-        company: "Spotify",
-        isTeachingRole: true,
-        role: "Backend Developer",
-      }}
-    />
-  </Box>
-);
+export const WorkHistory = () => {
+  const formRef = React.useRef<any>();
+  return (
+    <Box
+      m="10px auto"
+      w="100%"
+      shadow="0 0 2px 2px rgb(0 0 0 / 15%)"
+      borderRadius=".25rem"
+      maxW="600px"
+    >
+      <WorkHistoryComponent
+        handleSubmit={(values) => console.log(values)}
+        initialValues={{
+          company: "Spotify",
+          isTeachingRole: true,
+          role: "Backend Developer",
+          endYear: "2000",
+          startYear: "1994",
+          isCurrent: false,
+          showOnProfile: false,
+        }}
+        ref={formRef}
+      />
+      <IonButton expand="full" onClick={() => formRef.current.handleSubmit()}>
+        Submit
+      </IonButton>
+    </Box>
+  );
+};
