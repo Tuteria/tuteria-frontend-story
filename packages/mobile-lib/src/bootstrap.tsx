684fbece56d7b1296604e5d9cb73c32938870883
Malik Mahmud <almalikmahmud@gmail.com>
2021-07-24 15:56:25 +0100

    fix: add missing react import

--- packages/mobile-lib/src/bootstrap.tsx
+++ packages/mobile-lib/src/bootstrap.tsx
@@ -1,16 +1,10 @@
-import * as React from "react"
+import * as React from "react";
 // 1. import `ChakraProvider` component
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider } from "@chakra-ui/react";
 
-
-
-export const ThemeProvider:React.FC = ({ children })=> {
+export const ThemeProvider: React.FC = ({ children }) => {
   // 2. Use at the root of your app
-  return (
-    <ChakraProvider>
-      {children}
-    </ChakraProvider>
-  )
-}
+  return <ChakraProvider>{children}</ChakraProvider>;
+};
 
-export default ThemeProvider
+export default ThemeProvider;
