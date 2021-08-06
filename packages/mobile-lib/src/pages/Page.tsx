1c5156072e472576d320be1b23eeadc30367009f
usmanoa <usmanakorede@gmail.com>
2021-08-06 10:09:15 +0100

    merge origin

--- packages/mobile-lib/src/pages/Page.tsx
+++ packages/mobile-lib/src/pages/Page.tsx
@@ -1,3 +1,6 @@
+//@ts-ignore
+// @ts-nocheck
+
 import React from "react";
 import {
   IonButtons,
@@ -8,33 +11,88 @@ import {
   IonTitle,
   IonToolbar,
 } from "@ionic/react";
+import { Box, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
+import TutorPageWrapper from "../components/TutorPageWrapper";
+import { useHistory, useLocation } from "react-router";
+import { IRootStore } from "../store/types";
+import { observer } from "mobx-react-lite";
 
-const Page: React.FC<{ name?: string }> = ({ children, name }) => {
+const Skeletor = ({ children }) => {
+  return (
+    <Box padding="6" boxShadow="lg" bg="white">
+      <SkeletonCircle size="10" />
+      <SkeletonText mt="4" noOfLines={4} spacing="4" />
+    </Box>
+  );
+};
+
+const Page: React.FC<{
+  name?: string;
+  displayMenu?: boolean;
+  store: IRootStore;
+}> = ({ children, name = "", displayMenu = true, store }) => {
+  let history = useHistory();
+  let location = useLocation();
+  let options = {
+    Jobs: "/page/Jobs",
+    Subjects: "/page/Subjects",
+    Lessons: "",
+    Earnings: "",
+    Schedule: "/page/Schedule",
+  };
+  React.useEffect(() => {
+    store.setCurrentPage(name);
+  }, []);
+  function getKey() {
+    let result = Object.entries(options).find((z) => {
+      return z[1] === location.pathname;
+    });
+    return result ? result[0] : "";
+  }
+  function renderChildren() {
+    return displayMenu ? (
+      <React.Suspense fallback={<Skeletor />}>
+        <TutorPageWrapper
+          onSideBarItemClick={(item: any) => {
+            if (location.pathname !== options[item]) {
+              store.setCurrentPage(item);
+              history.push(options[item]);
+            }
+          }}
+          defaultMenu={store.currentPage}
+        >
+          {children}
+        </TutorPageWrapper>
+      </React.Suspense>
+    ) : (
+      children
+    );
+  }
   return (
     <IonPage>
-      {name ? (
+      {store.currentPage ? (
         <IonHeader>
           <IonToolbar>
             <IonButtons slot="start">
               <IonMenuButton />
             </IonButtons>
-            <IonTitle>{name}</IonTitle>
+            <IonTitle>{store.currentPage}</IonTitle>
           </IonToolbar>
         </IonHeader>
       ) : null}
 
       <IonContent fullscreen>
-        {name ? (
+        {store.currentPage ? (
           <IonHeader collapse="condense">
             <IonToolbar>
-              <IonTitle size="large">{name}</IonTitle>
+              <IonTitle size="large">{store.currentPage}</IonTitle>
             </IonToolbar>
           </IonHeader>
         ) : null}
-        {children}
+        {renderChildren()}
       </IonContent>
     </IonPage>
   );
 };
 
-export default Page;
+export default observer(Page);
