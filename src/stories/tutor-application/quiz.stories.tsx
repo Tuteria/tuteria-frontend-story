7ddd3c347d9c973898b600c7b7e003e31cc88910
Chidi Enwereji <chidibede@gmail.com>
2021-09-10 13:27:21 +0100

    fixed the submission of the quiz

--- src/stories/tutor-application/quiz.stories.tsx
+++ src/stories/tutor-application/quiz.stories.tsx
@@ -5,6 +5,7 @@ import DATA from "@tuteria/shared-lib/src/tutor-revamp/quizzes/sample-quiz-data"
 import QuizStore from "@tuteria/shared-lib/src/tutor-revamp/quizzes/quizStore";
 import React from "react";
 import "katex/dist/katex.min.css";
+import { linkTo } from "@storybook/addon-links";
 
 export default {
   title: "Tutor Application/Pages/Quiz",
@@ -17,14 +18,28 @@ export default {
   ],
 };
 
-const store = QuizStore.create({
-  questions: DATA.quiz.questions,
-});
+const store = QuizStore.create(
+  {
+    questions: DATA.quiz.questions,
+  },
+  {
+    postResults: async (answers: Array<any>) => {
+      return new Promise((resolve) => {
+        setTimeout(() => {
+          resolve(answers);
+        }, 3000);
+      });
+    },
+  }
+);
 
 export const Quiz = () => {
+  function redirect() {
+    linkTo("Tutor Application/Pages", "Tutor Page")();
+  }
   return (
     <Box>
-      <QuizPage index={0} store={store} onSubmit={async () => {}} />
+      <QuizPage index={0} store={store} onSubmit={redirect} />
     </Box>
   );
 };
