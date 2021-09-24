dcae02fb1d261279c7e37b8e1f68963b27e0e80a
chidibede <chidibede@gmail.com>
2021-09-24 16:15:20 +0100

    fetch subjects fix

--- src/stories/tutor-application/pages/index.stories.tsx
+++ src/stories/tutor-application/pages/index.stories.tsx
@@ -95,7 +95,7 @@ const adapter = {
     });
   },
   toNextPath: async () => {},
-  async fetchTutorSubjects() {
+  async getTutorSubjects() {
     let tutor_data = SAMPLE_TUTOR_SUBJECTS;
     return await new Promise((resolve) => {
       setTimeout(() => {
@@ -162,148 +162,148 @@ const store = RootStore.create(
   }
 );
 
-async function getTutorData() {
-  const data = {
-    userIsloggedIn: true,
-    locationInfo: {
-      country: "Nigeria",
-      regions: allRegions,
-      countries: allCountries,
-      state: "Lagos",
-      region: "Gbagada",
-      vicinity: "Charley boy Busstop",
-      address: "10, Lanre awolokun street",
-    },
-    personalInfo: {
-      firstName: "Abiola",
-      lastName: "Oyeniyi",
-      email: "james@example.com",
-      gender: "female",
-      nationality: "Nigeria",
-      dateOfBirth: "1998-10-12",
-      phone: "2347035209922",
-      whatsappNo: "2348152957065",
-      state: "Lagos",
-      vicinity: "Charley boy Busstop",
-      region: "Gbagada",
-      address: "Irabor Street Koto",
-      primaryLanguage: "English",
-      medium: "Facebook",
-    },
-    educationWorkHistory: {
-      educations: [
-        {
-          school: "Ikeja Grammar school",
-          country: "Nigeria",
-          course: "Chemistry",
-          degree: "MBBS",
-          speciality: "Mathematics",
-          startYear: "2006",
-          endYear: "2020",
-          grade: "First Class",
-        },
-        {
-          school: "University of Lagos",
-          country: "Nigeria",
-          course: "Organic Chemistry",
-          speciality: "Mathematics",
-          degree: "MBBS",
-          startYear: "2006",
-          endYear: "2020",
-          grade: "First Class",
-        },
-      ],
-      workHistories: [
-        {
-          company: "Tuteria Limited",
-          role: "CEO",
-          isTeachingRole: false,
-          startYear: "2015",
-          endYear: "2020",
-          isCurrent: true,
-          showOnProfile: true,
-        },
-      ],
-    },
-    subject: {
-      tutorSubjects: [
-        {
-          id: 1,
-          name: "General Mathematics",
-          category: "Academics",
-          subcategory: "Secondary",
-          status: "not-started",
-          title: "This is title from tutor subject for general mathematics",
-          description: "This is a description for general mathematics",
-          teachingStyle: "Terrorize my students",
-          trackRecords: "Cries everywhere",
-        },
-        {
-          id: 2,
-          name: "English",
-          category: "Academics",
-          subcategory: "Secondary",
-          status: "not-started",
-        },
-        {
-          id: 3,
-          name: "French",
-          category: "Academics",
-          subcategory: "Secondary",
-          status: "denied",
-        },
-        {
-          id: 4,
-          name: "Spanish",
-          category: "Academics",
-          subcategory: "Secondary",
-          status: "denied",
-        },
-        {
-          id: 5,
-          name: "Recognition",
-          category: "Academics",
-          subcategory: "Primary",
-          status: "not-started",
-        },
-        {
-          id: 5,
-          name: "Aptitude",
-          category: "Academics",
-          subcategory: "Adult",
-          status: "pending",
-          title: "This is title from tutor subject for Aptitude",
-          description: "This is a description for Aptitude",
-        },
-        {
-          id: 6,
-          name: "Speaking",
-          category: "Exam Prep",
-          subcategory: "IELTS",
-          status: "in-progress",
-        },
-        {
-          id: 7,
-          name: "Listening",
-          category: "Exam Prep",
-          subcategory: "IELTS",
-          status: "active",
-        },
-      ],
-    },
-    identity: {
-      profilePhotoId: "hello/holla",
-      profilePhoto:
-        "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
-    },
-    slug: "tutor-101",
-  };
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve(data);
-    }, 1000);
-  });
-}
+// async function getTutorData() {
+//   const data = {
+//     userIsloggedIn: true,
+//     locationInfo: {
+//       country: "Nigeria",
+//       regions: allRegions,
+//       countries: allCountries,
+//       state: "Lagos",
+//       region: "Gbagada",
+//       vicinity: "Charley boy Busstop",
+//       address: "10, Lanre awolokun street",
+//     },
+//     personalInfo: {
+//       firstName: "Abiola",
+//       lastName: "Oyeniyi",
+//       email: "james@example.com",
+//       gender: "female",
+//       nationality: "Nigeria",
+//       dateOfBirth: "1998-10-12",
+//       phone: "2347035209922",
+//       whatsappNo: "2348152957065",
+//       state: "Lagos",
+//       vicinity: "Charley boy Busstop",
+//       region: "Gbagada",
+//       address: "Irabor Street Koto",
+//       primaryLanguage: "English",
+//       medium: "Facebook",
+//     },
+//     educationWorkHistory: {
+//       educations: [
+//         {
+//           school: "Ikeja Grammar school",
+//           country: "Nigeria",
+//           course: "Chemistry",
+//           degree: "MBBS",
+//           speciality: "Mathematics",
+//           startYear: "2006",
+//           endYear: "2020",
+//           grade: "First Class",
+//         },
+//         {
+//           school: "University of Lagos",
+//           country: "Nigeria",
+//           course: "Organic Chemistry",
+//           speciality: "Mathematics",
+//           degree: "MBBS",
+//           startYear: "2006",
+//           endYear: "2020",
+//           grade: "First Class",
+//         },
+//       ],
+//       workHistories: [
+//         {
+//           company: "Tuteria Limited",
+//           role: "CEO",
+//           isTeachingRole: false,
+//           startYear: "2015",
+//           endYear: "2020",
+//           isCurrent: true,
+//           showOnProfile: true,
+//         },
+//       ],
+//     },
+//     subject: {
+//       tutorSubjects: [
+//         {
+//           id: 1,
+//           name: "General Mathematics",
+//           category: "Academics",
+//           subcategory: "Secondary",
+//           status: "not-started",
+//           title: "This is title from tutor subject for general mathematics",
+//           description: "This is a description for general mathematics",
+//           teachingStyle: "Terrorize my students",
+//           trackRecords: "Cries everywhere",
+//         },
+//         {
+//           id: 2,
+//           name: "English",
+//           category: "Academics",
+//           subcategory: "Secondary",
+//           status: "not-started",
+//         },
+//         {
+//           id: 3,
+//           name: "French",
+//           category: "Academics",
+//           subcategory: "Secondary",
+//           status: "denied",
+//         },
+//         {
+//           id: 4,
+//           name: "Spanish",
+//           category: "Academics",
+//           subcategory: "Secondary",
+//           status: "denied",
+//         },
+//         {
+//           id: 5,
+//           name: "Recognition",
+//           category: "Academics",
+//           subcategory: "Primary",
+//           status: "not-started",
+//         },
+//         {
+//           id: 5,
+//           name: "Aptitude",
+//           category: "Academics",
+//           subcategory: "Adult",
+//           status: "pending",
+//           title: "This is title from tutor subject for Aptitude",
+//           description: "This is a description for Aptitude",
+//         },
+//         {
+//           id: 6,
+//           name: "Speaking",
+//           category: "Exam Prep",
+//           subcategory: "IELTS",
+//           status: "in-progress",
+//         },
+//         {
+//           id: 7,
+//           name: "Listening",
+//           category: "Exam Prep",
+//           subcategory: "IELTS",
+//           status: "active",
+//         },
+//       ],
+//     },
+//     identity: {
+//       profilePhotoId: "hello/holla",
+//       profilePhoto:
+//         "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
+//     },
+//     slug: "tutor-101",
+//   };
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(data);
+//     }, 1000);
+//   });
+// }
 
 type TutorStoreType = {
   locationInfo: any;
@@ -425,6 +425,7 @@ export const TutorPage = () => {
     storage.set(adapter.regionKey, allRegions);
     storage.set(adapter.countryKey, allCountries);
     store.initializeTutorData(allRegions, allCountries, SAMPLE_TUTOR_DATA);
+    store.fetchTutorSubjects();
     setLoading(false);
   }, []);
 
