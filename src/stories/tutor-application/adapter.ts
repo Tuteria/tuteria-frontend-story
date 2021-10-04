1bb07712ab59a571c2de9fee7741b77d45d7bc6e
Biola <gbozee@example.com>
2021-10-04 12:10:26 +0100

    update the clean tuotr data

--- src/stories/tutor-application/adapter.ts
+++ src/stories/tutor-application/adapter.ts
@@ -36,9 +36,37 @@ type LocationInfoType = {
   address: string;
   country: string;
 };
+type EducationHistoryType = {
+  educations: Array<{
+    school: string;
+    country: string;
+    course: string;
+    degree: string;
+    startYear: string;
+    endYear: string;
+    grade: string;
+  }>;
+  workHistories: Array<{
+    company: string;
+    role: string;
+    isTeachingRole: boolean;
+    startYear: string;
+    endYear: string;
+    isCurrent: boolean;
+    showOnProfile: boolean;
+  }>;
+};
+type PasswordType = {};
+type SubjectSelectionType = {};
+type VerificationInfoType = {};
 type SubmissionType = {
   "personal-info": PersonalInfoType;
   "location-info": LocationInfoType;
+  "education-history": EducationHistoryType;
+  "work-history": EducationHistoryType;
+  "password-info": PasswordType;
+  "subject-selection": SubjectSelectionType;
+  verification: VerificationInfoType;
 };
 const adapter = {
   regionKey: REGION_KEY,
@@ -58,7 +86,7 @@ const adapter = {
     if (key === "personal-info") {
       result.personalInfo = { ...existingData.personalInfo, ...value };
     }
-    if (key == "location-info") {
+    if (key === "location-info") {
       let { exemptedAreas, lessonType, ...rest } = value;
       result.personalInfo = { ...existingData.personalInfo, ...rest };
       result.availability = {
@@ -66,6 +94,9 @@ const adapter = {
         exemptedAreas,
         preferredLessonType: lessonType,
       };
+    }
+    if (["education-history", "work-history"].includes(key)) {
+      result.educationWorkHistory = value;
     }
     return result;
   },
@@ -134,8 +165,7 @@ const adapter = {
             name: o.name,
             size: o.size?.toString(),
             public_id: "the_public_id",
-            url:
-              "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
+            url: "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
           }))
         );
       }, 2000);
