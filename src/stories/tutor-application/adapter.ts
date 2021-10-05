1455fd1e7e1c66d521bbc5343aeda8097255ecf7
Biola <gbozee@example.com>
2021-10-05 16:27:25 +0100

    accounted for the list of subjects

--- src/stories/tutor-application/adapter.ts
+++ src/stories/tutor-application/adapter.ts
@@ -22,20 +22,41 @@ export const testAdapter: ServerAdapterType = {
   saveTutorInfo: async (data: any) => {
     return await samplePromise();
   },
+  getTuteriaSubjects: () => {
+    return SAMPLE_TUTERIA_SUBJECTS;
+  },
   loadExistingTutorInfo: () => {
-    return SAMPLE_TUTOR_DATA;
+    return { ...SAMPLE_TUTOR_DATA, currentEditableForm: "subject-selection" };
   },
   updateUserPassword: async (password_data) => {
     return await samplePromise();
+  },
+  saveTutorSubjects: async (subjects) => {
+    let existingSubjects = SAMPLE_TUTOR_SUBJECTS.map((tx) => {
+      return {
+        id: tx.pk,
+        name: tx.skill.name,
+        category: tx.category,
+        status: tx.status,
+        title: tx.heading || "",
+        description: tx.description || "",
+        teachingStyle: tx.teachingStyle,
+        trackRecords: tx.trackRecords,
+      };
+    });
+    subjects.forEach((s) => {
+      existingSubjects.push({ id: 23, name: s, category: "Academics" });
+    });
+    return await samplePromise(existingSubjects);
   },
   getTutorSubjects: async () => {
     let tutor_data = SAMPLE_TUTOR_SUBJECTS;
     let result: {
       tutorSubjects: any[];
       tuteriaSubjects: any[];
-    } = await samplePromise({
+    } = await samplePromise(
       // tutorSubjects: [],
-      tutorSubjects: tutor_data.map((tx) => {
+      tutor_data.map((tx) => {
         return {
           id: tx.pk,
           name: tx.skill.name,
@@ -46,9 +67,8 @@ export const testAdapter: ServerAdapterType = {
           teachingStyle: tx.teachingStyle,
           trackRecords: tx.trackRecords,
         };
-      }),
-      tuteriaSubjects: SAMPLE_TUTERIA_SUBJECTS,
-    });
+      })
+    );
     return result;
     // if session storage exists return the tuteria subjects else fetch
   },
@@ -67,8 +87,7 @@ export const testAdapter: ServerAdapterType = {
         name: o.name,
         size: o.size?.toString(),
         public_id: "the_public_id",
-        url:
-          "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
+        url: "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
       }))
     );
   },
