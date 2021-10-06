2dd34dcd0f20e781c5c87afbcbbe411bdd794d56
Biola <gbozee@example.com>
2021-10-06 15:32:14 +0100

    fixed tutor components

--- src/stories/tutor-application/adapter.ts
+++ src/stories/tutor-application/adapter.ts
@@ -26,7 +26,7 @@ export const testAdapter: ServerAdapterType = {
     return SAMPLE_TUTERIA_SUBJECTS;
   },
   loadExistingTutorInfo: () => {
-    return SAMPLE_TUTOR_DATA;
+    return { ...SAMPLE_TUTOR_DATA, currentEditableForm: "subject-selection" };
   },
   updateUserPassword: async (password_data) => {
     return await samplePromise();
@@ -87,8 +87,7 @@ export const testAdapter: ServerAdapterType = {
         name: o.name,
         size: o.size?.toString(),
         public_id: "the_public_id",
-        url:
-          "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
+        url: "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
       }))
     );
   },
