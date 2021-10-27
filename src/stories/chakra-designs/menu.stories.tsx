a4f12fab74b00e4f2918e0b12847f5c265274e6d
chidibede <chidibede@gmail.com>
2021-10-27 15:45:49 +0100

    ported chakra pro menu

---
+++ src/stories/chakra-designs/menu.stories.tsx
@@ -0,0 +1,34 @@
+import { Box } from "@chakra-ui/react";
+import ThemeProvider from "@tuteria/shared-lib/src/bootstrap";
+import SimpleWithBadgeComponent from "@tuteria/shared-lib/src/components/chakra-pro/Menu/SimpleWithBadge";
+import WithStartIcon from "@tuteria/shared-lib/src/components/chakra-pro/Menu/WithStartIcon";
+import React from "react";
+
+export default {
+  title: "Components/Chakra Pro/Menu",
+  decorators: [
+    (Story: React.FC) => (
+      <ThemeProvider>
+        <Box
+          h="100vh"
+          bg="white"
+          overflowX="hidden"
+          css={{
+            "&::-webkit-scrollbar": {
+              display: "hidden",
+            },
+          }}
+        >
+          <Story />
+        </Box>
+      </ThemeProvider>
+    ),
+  ],
+};
+
+export const SimpleWithBadge = () => {
+  return <SimpleWithBadgeComponent />;
+};
+export const MenuWithStartIcon = () => {
+  return <WithStartIcon />;
+};
