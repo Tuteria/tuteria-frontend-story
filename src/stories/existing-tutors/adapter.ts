220119aafb06d94213729a1855e4cdfb52e985e5
Malik Mahmud <almalikmahmud@gmail.com>
2021-11-29 15:17:13 +0100

    chore: refactor existing-tutors stories

--- src/stories/existing-tutors/adapter.ts
+++ src/stories/existing-tutors/adapter.ts
@@ -1,9 +1,3 @@
-import {
-  AdapterType,
-  ServerAdapterType,
-} from "@tuteria/shared-lib/src/adapter";
-import BANK_DATA from "@tuteria/shared-lib/src/data/banks.json";
-import educationWorkData from "@tuteria/shared-lib/src/data/educationData.json";
 import DATA, {
   SAMPLE_QUIZ_DATA,
 } from "@tuteria/shared-lib/src/data/sample-quiz-data";
@@ -16,7 +10,6 @@ import {
   SAMPLE_TUTOR_SUBJECTS,
   SUBJECT_GROUPS,
   TEACHING_PREFERENCES,
-  TUTORS,
 } from "@tuteria/shared-lib/src/data/tutor-application/sample_data";
 import storage from "@tuteria/shared-lib/src/local-storage";
 import { uploadToCloudinary } from "@tuteria/shared-lib/src/utils";
@@ -28,64 +21,18 @@ function samplePromise(data = {}, timer = 300): Promise<any> {
     }, 300);
   });
 }
-const formIds = {
-  1: "personal-info",
-  2: "location-info",
-  3: "education-history",
-  4: "work-history",
-  7: "schedule-info",
-  8: "agreement-info",
-  9: "guarantors-info",
-  10: "payment-info",
-  11: "new-development-info",
-  12: "teaching-profile",
+
+export const subjectPageData = {
+  educationWorkHistory: SAMPLE_TUTOR_DATA.educationWorkHistory,
+  subjectData: {
+    tutorSubjects: SAMPLE_TUTOR_SUBJECTS,
+    tuteriaSubjects: SAMPLE_TUTERIA_SUBJECTS,
+    supportedCountries,
+    groups: SUBJECT_GROUPS,
+  },
 };
 
-function loadExistingTutorInfo() {
-  return {
-    ...SAMPLE_TUTOR_DATA,
-    appData: { currentEditableForm: formIds[10] },
-  };
-}
-
-const clearSubjectDescription = () => {
-  storage.clear("SUBJECT_DESCRIPTION");
-  storage.clear("TEACHING_STYLE");
-  storage.clear("TRACK_RECORD");
-};
-
-const initializeApplication = async (
-  adapter: AdapterType,
-  { regions, countries, tuteriaSubjects }
-) => {
-  storage.set(adapter.regionKey, regions);
-  storage.set(adapter.countryKey, countries);
-  storage.set(adapter.supportedCountriesKey, supportedCountries);
-  storage.set(adapter.tuteriaSubjectsKey, tuteriaSubjects);
-  return await samplePromise({
-    staticData: {
-      regions,
-      countries,
-      supportedCountries,
-      educationData: {
-        degree_data: educationWorkData.degree_data,
-        grade_data: educationWorkData.grade_data,
-        specialities: educationWorkData.specialities,
-        sources: educationWorkData.sources,
-        languages: educationWorkData.languages,
-      },
-    },
-    tutorInfo: loadExistingTutorInfo(),
-    subjectData: {
-      tutorSubjects: SAMPLE_TUTOR_SUBJECTS,
-      tuteriaSubjects: tuteriaSubjects,
-      supportedCountries,
-      groups: SUBJECT_GROUPS,
-    },
-  });
-};
-
-export const testAdapter: ServerAdapterType = {
+export const testAdapter: any = {
   getSubjectData() {
     return {
       tutorSubjects: SAMPLE_TUTOR_SUBJECTS,
@@ -101,14 +48,6 @@ export const testAdapter: ServerAdapterType = {
   },
   getTuteriaSubjects: () => {
     return SAMPLE_TUTERIA_SUBJECTS;
-  },
-  fetchBanksInfo: async (supportedCountryName) => {
-    let response = BANK_DATA[supportedCountryName].map((bank) => bank.name);
-    return await samplePromise(response);
-  },
-  loadExistingTutorInfo,
-  updateUserPassword: async (password_data) => {
-    return await samplePromise();
   },
   saveTutorSubjects: async (subjects) => {
     let existingSubjects: any = SAMPLE_TUTOR_SUBJECTS.map((tx) => {
@@ -137,6 +76,7 @@ export const testAdapter: ServerAdapterType = {
     // clearSubjectDescription();
     return await samplePromise({ values, subject_id });
   },
+  saveSubject(subject_id, subject) {},
   async checkSpellingAndGrammar(checks: any[]) {
     /**
      * checks = [{key:"description","value":"Thi is aia amaple"}]
@@ -182,32 +122,11 @@ export const testAdapter: ServerAdapterType = {
   fetchQuizQuestions: async (quizSubjects) => {
     return await samplePromise({ quiz: DATA.quiz, quizSubjects });
   },
-  fetchLandingPageTutors: async () => {
-    return await samplePromise(TUTORS);
-  },
-  saveSubject(subject_id, subject) {},
+
   loadExistingSubject(subject_id) {
     return SAMPLE_TUTOR_SUBJECTS[0];
   },
-  async uploadAndVerifyProfile(uploadedFile) {
-    let { slug } = loadExistingTutorInfo();
-    // this is useful for the parameters to send to cloudinary
-    let folder = "profile_pics";
-    let checkQuality = true;
-    let uniqudId = `${slug}-profile-pic`;
-    let result = await samplePromise([
-      {
-        public_id: slug,
-        url: "https://images.unsplash.com/photo-1502378735452-bc7d86632805?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=aa3a807e1bbdfd4364d1f449eaa96d82",
-        quality: false,
-      },
-    ]);
-    return {
-      profile_id: result[0].public_id,
-      url: result[0].url,
-      quality: true,
-    };
-  },
+
   uploadApiHandler: async (files: any[]) => {
     return await samplePromise(
       files.map((o) => ({
@@ -233,9 +152,6 @@ export const testAdapter: ServerAdapterType = {
     );
     return Promise.all(promises);
   },
-  generateQuiz: async (subjects: any) => {
-    return await samplePromise([]);
-  },
   submitQuizResults: async (payload) => {
     return await samplePromise({ payload });
   },
@@ -257,37 +173,7 @@ export const testAdapter: ServerAdapterType = {
   beginQuiz: async (payload) => {
     return await samplePromise({});
   },
-  sendEmailVerification: async ({ email, code }) => {
-    if (code) {
-      return await samplePromise({ status: "Email verified", verified: true });
-    }
-    return await samplePromise(undefined);
-  },
-  submitVideoRecording: async (url) => {
-    return await samplePromise({
-      id: "sample-video",
-      url: "https://www.youtube.com/watch?v=sVPYIRF9RCQ",
-    });
-  },
-  initializeSubject: async (adapter, subjectInfo, key) => {
-    let response = await initializeApplication(adapter, {
-      regions: [],
-      countries: [],
-      tuteriaSubjects: SAMPLE_TUTERIA_SUBJECTS,
-    });
-    let foundSubject = {
-      ...SAMPLE_TUTOR_SUBJECTS[0],
-      quizzes: subjectInfo.subjects,
-    };
-    return { foundSubject, response };
-  },
-  initializeApplication,
-  authenticateUser: async ({ email, otp }) => {
-    return await samplePromise({ email, otp });
-  },
-  createQuizFromSheet: async () => {
-    return await samplePromise({});
-  },
+
   buildPreferences(subject: { category?: string; name?: string }) {
     if (subject.category === "Academics") {
       return [
